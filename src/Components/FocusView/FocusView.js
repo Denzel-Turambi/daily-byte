77071a7dbca41795957d56e880d7f900ac353eb2
Denzel Turambi <dturambi26@gmail.com>
2023-11-02 20:43:03 -0600

    feat: Add styling to focus page

--- src/Components/FocusView/FocusView.js
+++ src/Components/FocusView/FocusView.js
@@ -1,3 +1,4 @@
+import './FocusView.css';
 import { Link, useParams } from "react-router-dom";
 
 function FocusView({ news }) {
@@ -5,20 +6,21 @@ function FocusView({ news }) {
   const id = useParams();
 
   const singleArticle = news.find(article => id.id === article.title)
-  console.log(singleArticle)
 
   return (
-    <div>
-      <h1>{singleArticle.title}</h1>
-      <h3>Written by {singleArticle.author}</h3>
-      <p>Published on {singleArticle.publishedAt}</p>
+    <div className="focus-container">
+      <Link to="/" className="button-container">
+        <p className="back-button">Go Back</p>
+      </Link>
+      <div className="article-header">
+        {!singleArticle.title ? <h1>No Title</h1> : <h1>{singleArticle.title}</h1>}
+        {!singleArticle.author ? <h3>Anonymous Writer</h3> : <h3>Written by {singleArticle.author}</h3>}
+        <p>Published on {singleArticle.publishedAt}</p>
+      </div>
       <p>{singleArticle.description}</p>
-      <img className="dashboard-img" src={singleArticle.urlToImage} alt="news article image" />
+      <img className="focus-img" src={singleArticle.urlToImage} alt="news article image" />
       <p>{singleArticle.content}</p>
       <p>To read the full article, click <a href={singleArticle.url}>here</a></p>
-      <Link to="/">
-        <button>Go Back</button>
-      </Link>
     </div>
   )
 }
